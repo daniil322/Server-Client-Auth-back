35fd971640b853a82f82ad8079b025e6aba689d6
Daniil Glezer <daniilglezer@Daniils-MacBook-Pro.local>
2020-04-24 00:11:32 +0300

    fixed heroku

--- build/server.js
+++ build/server.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var body_parser_1 = __importDefault(require("body-parser"));
+var cors_1 = __importDefault(require("cors"));
 var path_1 = __importDefault(require("path"));
 var user_routes_1 = __importDefault(require("./api/user/user.routes"));
 var charts_routes_1 = __importDefault(require("./api/chart/charts.routes"));
@@ -12,10 +13,21 @@ var app = express_1.default();
 var http = require("http").createServer(app);
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
-app.use(express_1.default.static(path_1.default.resolve(__dirname, "public")));
-app.get("/*", function (req, res) {
-    res.sendFile(path_1.default.join(__dirname, "public", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+    app.use(express_1.default.static(path_1.default.resolve(__dirname, "public")));
+}
+else {
+    var corsOptions = {
+        origin: [
+            "http://127.0.0.1:8080",
+            "http://localhost:8080",
+            "http://127.0.0.1:3000",
+            "http://localhost:3000",
+        ],
+        credentials: true,
+    };
+    app.use(cors_1.default(corsOptions));
+}
 app.use("/api/user", user_routes_1.default);
 app.use("/api/charts", charts_routes_1.default);
 var port = process.env.PORT || 3030;
--- src/server.ts
+++ src/server.ts
@@ -12,11 +12,20 @@ var http = require("http").createServer(app);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(path.resolve(__dirname, "public")));
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "public")));
+} else {
+  const corsOptions = {
+    origin: [
+      "http://127.0.0.1:8080",
+      "http://localhost:8080",
+      "http://127.0.0.1:3000",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
+}
 
 app.use("/api/user", userRoutes);
 app.use("/api/charts", chartsRoutes);
