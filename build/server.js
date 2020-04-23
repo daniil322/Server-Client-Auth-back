32cb636dab87a975ccf96ad603cf458aa1a15790
Daniil Glezer <daniilglezer@Daniils-MacBook-Pro.local>
2020-04-23 23:41:32 +0300

    fixed heroku

--- build/server.js
+++ build/server.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var body_parser_1 = __importDefault(require("body-parser"));
-var cors_1 = __importDefault(require("cors"));
 var path_1 = __importDefault(require("path"));
 var user_routes_1 = __importDefault(require("./api/user/user.routes"));
 var charts_routes_1 = __importDefault(require("./api/chart/charts.routes"));
@@ -13,10 +12,9 @@ var app = express_1.default();
 var http = require("http").createServer(app);
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
-
-    app.use(express_1.default.static(path_1.default.resolve(__dirname, "public")));
-
-
+app.get('/*', function (req, res) {
+    res.sendFile(path_1.default.resolve(__dirname + '/build/index.html'));
+});
 app.use("/api/user", user_routes_1.default);
 app.use("/api/charts", charts_routes_1.default);
 var port = process.env.PORT || 3030;
--- src/server.ts
+++ src/server.ts
@@ -12,15 +12,10 @@ var http = require("http").createServer(app);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "public")));
-} else {
-  const corsOptions = {
-    origin: ["http://127.0.0.1:3000", "http://localhost:3000"],
-    credentials: true,
-  };
-  app.use(cors(corsOptions));
-}
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/build/index.html'))
+})
+
 
 app.use("/api/user", userRoutes);
 app.use("/api/charts", chartsRoutes);
