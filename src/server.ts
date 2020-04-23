270879a032a5b28f4c2bc45c5e89aae22d8987e9
Daniil Glezer <daniilglezer@Daniils-MacBook-Pro.local>
2020-04-23 23:42:58 +0300

    fixed heroku

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname + '/build/index.html'))
+  res.sendFile(path.resolve(__dirname + '/build/public/index.html'))
 })
 
 
