013d9f7893e93f1ecb8771bbbad0da83317553e4
Daniil Glezer <daniilglezer@Daniils-MacBook-Pro.local>
2020-04-24 09:32:30 +0300

    fixed heroku

---
+++ build/public/precache-manifest.fc3165701b3db7826d759dfb03cbdd16.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1bcc1fd063f900fb4e29ebb12c2ecf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84b7940916242c9ae148",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "cba59380cb2a6f8c1d96",
+    "url": "/static/css/main.0fa7ec49.chunk.css"
+  },
+  {
+    "revision": "84b7940916242c9ae148",
+    "url": "/static/js/2.0964820e.chunk.js"
+  },
+  {
+    "revision": "cba59380cb2a6f8c1d96",
+    "url": "/static/js/main.de724c51.chunk.js"
+  },
+  {
+    "revision": "bf19f19c957b2f4c73a3",
+    "url": "/static/js/runtime~main.2461793c.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
