a002d1ea17477e869c0c5ee402e204a37a8408e1
Daniil Glezer <daniilglezer@Daniils-MacBook-Pro.local>
2020-04-24 08:16:29 +0300

    fixed heroku

--- build/public/service-worker.js
+++ build/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/material-dashboard-react/precache-manifest.7efd535dd37b7d043bd9d54e047c103d.js"
+  "/precache-manifest.4c2bdc02334819ecc6e238d57f752652.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/material-dashboard-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
